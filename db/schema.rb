0cc36a9f8801f0c7535fe98bc012cbe9b5fefdd6
new-maker-1 <new-maker-1@outlook.com>
2017-04-05 14:49:58 -0700

    all

--- db/schema.rb
+++ db/schema.rb
@@ -179,12 +179,12 @@ ActiveRecord::Schema.define(version: 20170326025347) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -193,10 +193,7 @@ ActiveRecord::Schema.define(version: 20170326025347) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
-    t.string   "confirmation_token",     limit: 128
-    t.string   "remember_token",         limit: 128
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
